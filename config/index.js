05a2b239a5f9db182344a24b9ac518e770c3da65
郭荣 <2293018816@qq.com>
2021-03-28 10:12:04 +0800

    登录页面初次完成

--- config/index.js
+++ config/index.js
@@ -64,7 +64,8 @@ const config = {
           generateScopedName: '[name]__[local]___[hash:base64:5]'
         }
       }
-    }
+    },
+    esnextModules: ['taro-ui']
   }
 }
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,57 @@
-import Taro from '@tarojs/taro'
 import React from 'react' 
-import { useAsyncEffect } from '../../utils/custom_hooks'
-import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { View} from '@tarojs/components'
+import { useAsyncEffect } from '@/src/utils/custom_hooks'
+import { AtInput,AtButton }  from 'taro-ui'
 
+import './index.scss'
 
 function Index () {
 
   useAsyncEffect(async () => {
-
+    console.log('useeffect方发fffff')
   },[])
 
+  const mobileChange = ()=>{
+
+  }
+  const codeChange = ()=>{
+
+  }
+
   return (
-    <View className='index'>你好呀</View>
+    <View className='index'>
+      {/* 背景图 */}
+      <View className='m-loginbg'>
+        {/* 欢迎登录 */}
+        <View className='m-title'>欢迎登录</View>
+
+        {/* 登录 */}
+        <View className='m-login-input'>
+          <AtInput
+            className='mobile_input'
+            name='value1'
+            type='text'
+            placeholder='请输入手机号'
+            onChange={mobileChange}
+          />
+          <AtInput
+            className='mobile_input'
+            type='text'
+            maxLength='4'
+            placeholder='验证码'
+            value=''
+            onChange={codeChange}
+          >
+            发送验证码
+      </AtInput>
+          <AtButton type='primary' size='normal' 	className='loginButton'>立即登录</AtButton>
+        </View>
+      </View>
+    
+
+    </View>
   )
 }
 
-export default Index
+export default Index      
